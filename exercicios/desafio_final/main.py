2268e7ccbb76674960e2ade31bb5e6c6c0dfa63a
Raiane Cardoso <raianecardoso.rede@gmail.com>
2026-09-01 14:05:09 -0300

    Resolve desafio final.

--- exercicios/desafio_final/main.py
+++ exercicios/desafio_final/main.py
@@ -1,4 +1,16 @@
 def resposta(mensagem, chave):
-    for letra in mensagem: 
-        if letra.isalpha():
-            pass
+    resultado = ""
+
+    for char in mensagem:
+        if char.isalpha():
+            posicao = ord(char) - ord("A")
+            posicao = (posicao - chave) % 26
+            nova_letra = chr(posicao + ord("A"))
+            resultado += nova_letra
+        else:
+            resultado += char
+    return resultado
+
+mensagem_decodificada = resposta("MXOXYBKP, ! SLZB ZLKZIRFR L ZROPL QOFIEX ABS MVQELK", 23)
+
+print(mensagem_decodificada)
